d322081eb8cf1363586c6e0e3ed66749a40ee0de
Baran Sülü <73203587+Barannsulu@users.noreply.github.com>
2022-01-25 17:52:42 +0300

    Update FeedItem.jsx

--- src/components/FeedItem.jsx
+++ src/components/FeedItem.jsx
@@ -30,29 +30,25 @@ const FeedItem = ({
                         <div className='flex items-center justify-center w-8 h-8 rounded-full group-hover:bg-primary-light '>
                             <ReplyIcon className='w-5 h-5 group-hover:text-primary-base'  />
                         </div>
-                        <span className='group-hover:text-primary-base'>7</span>
                     </li>
 
                       <li className='group flex items-center  space-x-3 text-gray-dark text-sm '>
                         <div className='flex items-center justify-center w-8 h-8 rounded-full group-hover:bg-green-200 '>
                             <ReTweetIcon className='w-5 h-5 group-hover:text-green-400'  />
                         </div>
-                        <span className='group-hover:text-primary-base'>7</span>
-                    </li>
+                      </li>
 
                       <li className='group flex items-center  space-x-3 text-gray-dark text-sm '>
                         <div className='flex items-center justify-center w-8 h-8 rounded-full group-hover:bg-pink-200 '>
                             <LikeIcon className='w-5 h-5 group-hover:text-pink-400'  />
                         </div>
-                        <span className='group-hover:text-primary-base'>7</span>
-                    </li>
+                       </li>
 
                      <li className='group flex items-center  space-x-3 text-gray-dark text-sm '>
                         <div className='flex items-center justify-center w-8 h-8 rounded-full group-hover:bg-primary-light '>
                             <ShareIcon className='w-5 h-5 group-hover:text-primary-base'  />
                         </div>
-                        <span className='group-hover:text-primary-base'>7</span>
-                    </li>
+                     </li>
 
                     
                 </ul>
